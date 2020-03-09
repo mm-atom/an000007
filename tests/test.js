6551b058c918cf0c2c78d046ab518bb80019187f
taoqf <tao_qf@163.com>
2020-03-09 12:36:11 +0800

    add tests

--- tests/test.js
+++ tests/test.js
@@ -2,6 +2,25 @@ const test = require('ava');
 
 const { default: a } = require('../dist/index');
 
-test('xxx', (t) => {
-	t.pass();
+test('get url param => without default', (t) => {
+	const mm = {
+		data: {
+			params: {
+				'foo': 'bar'
+			}
+		}
+	};
+	const v = a(mm, 'foo', 'mmstudio');
+	t.is(v, 'bar');
+});
+
+test('get url param => with default', (t) => {
+	const mm = {
+		data: {
+			params: {
+			}
+		}
+	};
+	const v = a(mm, 'foo', 'mmstudio');
+	t.is(v, 'mmstudio');
 });
