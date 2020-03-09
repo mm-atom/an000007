a47c394c6b35fe1e97f54ed569c33fd7ed943659
taoqf <tao_qf@163.com>
2020-03-09 11:23:57 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import aw2 from '@mmstudio/an000002';
 
-export default function an7(param1: string, param2: string, param3: string) {
+export default function an7(mm: aw2, key: string, default_value?: string) {
+	return mm.data.params[key] || default_value;
 }
